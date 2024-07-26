88cb7188a984cf8785ce8483b1e913bd31d49e22
Divaish6 <Diivaish28@gmail.com>
2024-07-26 13:30:10 +0100

    adding file lab008

---
+++ src/day13072024/Lab008.java
@@ -0,0 +1,60 @@
+package day13072024;
+
+import javax.print.DocFlavor;
+
+public class Lab008 {
+    public static <string> void main(String[] args) {
+
+        // data_type variable_name = variable_value(literal)
+
+        // Data Type
+        // Primitive Data Type
+        //  boolean
+            // true or false
+        boolean is_married = true;
+        boolean are_you_a_dog = false;
+        System.out.println(is_married);
+        System.out.println(are_you_a_dog);
+
+
+        // Numerical
+           // integral - byte, short, int, long
+
+        byte b = 100;
+        short s = 101;
+        int age = 130;
+        long phone_number = 742485559; // 742485559;
+
+             // char - char - A,B
+        char grade = 'A'; // O,A,B,C,D,E,F = fail
+
+        // floating
+           // float
+        float pi = 3.14f; // 3.14f;
+           // double
+        double d = 118.654;
+
+
+        // Non-primitive data types
+        // string name -> "visualise"; -, number of char or a bunch of char
+        string name = (string) "vaishali";
+          String cc = "c";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
