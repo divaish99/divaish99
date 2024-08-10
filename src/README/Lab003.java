8bd25cab8d542a08a94a0debe54657edda613b82
Divaish6 <Diivaish28@gmail.com>
2024-08-10 15:25:35 +0100

    add file Lab003

---
+++ src/README/Lab003.java
@@ -0,0 +1,11 @@
+package README;
+
+import java.security.Principal;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("vaishali");
+        System.out.println("parkas");
+
+    }
+}
