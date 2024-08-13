63deda6164868c67165d9ce68cc301ee0406a7c6
Divaish6 <Diivaish28@gmail.com>
2024-08-13 14:42:23 +0100

    file lab001

--- src/ex07062024/Lab002.java
+++ src/ex07062024/Lab002.java
@@ -3,5 +3,6 @@ package ex07062024;
 public class Lab002 {
     public static void main(String[] args) {
         System.out.println("Hello World Vaishali!");
+        System.out.println("Hello Divyesh!");
     }
 }
